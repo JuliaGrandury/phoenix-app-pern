56ca18028ff01111483690e54f245a9d14b5175e
juliagrandury <juliagrandury@wustl.edu>
2022-11-24 18:31:46 -0500

    Normalizing styling across app pages

--- frontend/client/src/components/Dashboard/Dashboard.jsx
+++ frontend/client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,13 @@
+import styles from './Dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <>
-      <div>Welcome to your User</div>
+    <div className={styles.PageContainer}>
+      <h1>Welcome to your dashboard Julia</h1>
       <div className="top-left">Job Application Statistics</div>
       <div className="top-right">Networking Statistics</div>
       <div className="bottom">Progress Map</div>
-    </>
+    </div>
   )
 }
 
