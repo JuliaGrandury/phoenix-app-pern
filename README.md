2bf5e4c80b97fa49f3837034f9cb6a8576dba52f
juliagrandury <juliagrandury@wustl.edu>
2022-11-24 23:47:18 -0500

    Designing Network Data DB Schema

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+Features in Development
+- [ ] Accounting for Job Url in Job Board
+- [ ] Account for Job Url and Company Url in WriteRows.jsx
+- [ ] Allowing for import of CSV data in Job Board
+- [ ] Notifications for Job Board and Network Graph
+- [ ] Authentication and Authorization and with that the Account Settings
+- [ ] Add front end and back end testing
--- backend/server/database.sql
+++ backend/server/database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE phoenixapp;
 
 CREATE TABLE jobs (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULLABLE,
     jobTitle TEXT NOT NULLABLE,
     jobLink TEXT NOT NULLABLE,
     jobCity VARCHAR NOT NULLABLE,
@@ -26,9 +26,31 @@ CREATE TABLE users (
     createdAt timestamp NOT NULLABLE
 );
 
+CREATE TABLE connections (
+    id SERIAL PRIMARY KEY NOT NULLABLE,
+    fullname TEXT NOT NULLABLE, 
+    role TEXT NOT NULLABLE,
+    company TEXT NOT NULLABLE,
+    contactDates DATE [] NOT NULLABLE,
+    contactInformation {phone: TEXT NOT NULLABLE, email: TEXT NOT NULLABLE, linkedin: TEXT NOT NULLABLE},
+    offers TEXT [] NOT NULLABLE,
+    secondaryConnections TEXT [] NOT NULLABLE,
+    homework: TEXT [] NOT NULLABLE,
+    status TEXT NOT NULLABLE,
+)
 
 -- add indexing to jobTitle, jobLocation, companyName and appStatus
--- think of structure for network
--- each person would have an associated job title, company, date of call, origin, transcript
-    -- did they offer Mock Interview, Referral, Connect to Network, Resume Advice
-    -- connections to other users
+
+-- think of structure for network (colored status dots with name, title, company that you can click on for a full card to appear)
+-- each CONNECTION HAS the following card
+    -- full name : Liam Jolley
+    -- role : Software Engineer II
+    -- company : X The Moonshot Factory
+    -- contactDates : [first call, second call, etc]
+    -- contactInformation: { phone : 917849234/NULL, email: test@gmail/NULL, linkedin : linkedin.com/NULL}
+    -- offers: advice (link to notes), mock interview, list of suggested roles/companies, referral
+    -- secondaryConnections: [Julia Lawrence, Melody Gibson, etc.]
+    -- homework: Work on ..., Schedule Mock Interview, Send Resume
+    -- status: Active, Inactive (becomes inactive after 4 weeks of no comm)
+
+
