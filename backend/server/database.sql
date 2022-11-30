cc3b0db757efa02b83d838097e954b842ec6a37f
juliagrandury <juliagrandury@wustl.edu>
2022-11-29 21:04:05 -0500

    Fixed DB schemas and corresponding API requests for jobs and connections

--- backend/server/database.sql
+++ backend/server/database.sql
@@ -5,30 +5,37 @@ CREATE TABLE users (
     username TEXT NOT NULL,
     email TEXT NOT NULL,
     password TEXT NOT NULL,
-    created_at timestamp NOT NULL,
+    created_at TIMESTAMP,
     UNIQUE (username)
 );
 
 CREATE TABLE jobs (
     id SERIAL PRIMARY KEY NOT NULL,
-    role TEXT NOT NULL,
-    role_link TEXT NOT NULL,
+    role VARCHAR NOT NULL,
+    role_link VARCHAR,
     city VARCHAR NOT NULL,
+    state VARCHAR,
     country VARCHAR NOT NULL,
-    company TEXT NOT NULL,
-    company_link TEXT NOT NULL,
+    company VARCHAR NOT NULL,
+    company_link VARCHAR,
     description VARCHAR NOT NULL,
-    connections [],
-    status TEXT NOT NULL,
-    applied_on DATE NOT NULL,
-    created_at timestamp NOT NULL,
-    FOREIGN KEY(connections) REFERENCES connections(id)
+    connections VARCHAR [],
+    status VARCHAR NOT NULL,
+    applied_on DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- because contact_dates is an array of "dates" to "note_link" pairs
-CREATE EXTENSION HSTORE;
+INSERT INTO jobs (role, role_link, city, state, country, company, company_link, description, connections, status, applied_on, created_at) 
+VALUES ('Software Engineer', 'https://www.accenture.com/us-en/careers/jobdetails?id=R00120688_en&title=Federal+-+Software+Engineer+Analyst', 'New York', 'NY', 'USA', 'Accenture', 'https://accenture.com', 'bla bla', '{"Charles", "Emma", "Oscar"}', 'Applied', '2022-12-02', '2017-05-25 17:39:15.014961-05');
 
--- making my own composite type for contact information
+INSERT INTO jobs (role, role_link, city, state, country, company, company_link, description, connections, status, applied_on, created_at) 
+VALUES ('Software Engineer II', 'https://www.whoop.com/careers', 'Boston', 'MA', 'USA', 'Whoop', 'https://whoop.com', 'healthy', '{"Sasha", "Jake", "Pauline"}', 'To Apply', null, '2017-05-25 17:39:15.014961-05');
+
+INSERT INTO jobs (role, role_link, city, state, country, company, company_link, description, connections, status, applied_on) 
+VALUES ('Software Engineer II', 'https://www.whoop.com/careers', 'Boston', 'MA', 'USA', 'Whoop', 'https://whoop.com', 'healthy', '{"Sasha", "Jake", "Pauline"}', 'To Apply', null);
+
+
+-- Created a composite type for contact information
 CREATE TYPE contactinfo_item AS (
     phone TEXT,
     email TEXT,
@@ -40,35 +47,36 @@ CREATE TABLE connections (
     fullname TEXT NOT NULL, 
     role TEXT NOT NULL,
     company TEXT NOT NULL,
-    contact_dates hstore[] NOT NULL,
+    contact_dates hstore,
     contact_information contactinfo_item,
-    offers TEXT[] NOT NULL,
-    secondary_cons TEXT[] NOT NULL,
-    homework TEXT[] NOT NULL,
+    offers TEXT[],
+    secondary_cons TEXT[],
+    homework TEXT[],
     status TEXT NOT NULL
 );
-
--- m to m table of users to other users
-CREATE TABLE granted_accesses (
-)
-
-INSERT INTO connections (fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status) 
-VALUES ('Liam Jolley', 'Software Engineer', '{"Dec 9"=>"google.com", "Dec 10"=>"linkedincom", "Nov 20"=>"bla", "Nov 24"=>"google.com"}', 
-'ROW("9174235567", "test@gmail.com", "louie@linkedin.com")', '["Resume Review", "]'), ;
 
 
 -- add indexing to jobTitle, jobLocation, companyName and appStatus
 
--- think of structure for network (colored status dots with name, title, company that you can click on for a full card to appear)
--- each CONNECTION HAS the following card
-    -- full name : Liam Jolley
-    -- role : Software Engineer II
-    -- company : X The Moonshot Factory
-    -- contactDates : [first call, second call, etc]
-    -- contactInformation: { phone : 917849234/NULL, email: test@gmail/NULL, linkedin : linkedin.com/NULL}
-    -- offers: advice (link to notes), mock interview, list of suggested roles/companies, referral
-    -- secondaryConnections: [Julia Lawrence, Melody Gibson, etc.]
-    -- homework: Work on ..., Schedule Mock Interview, Send Resume
-    -- status: Active, Inactive (becomes inactive after 4 weeks of no comm)
+
+-- INSERT STATEMENTS FOR connections TABLE (working)
+
+-- INSERT INTO connections (fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status) 
+-- VALUES ('Liam Jolley', 'Software Engineer', 'X The Moonshot Factory', 
+-- ' "Dec 9" => "google.com", "Dec 10" => "linkedincom", "Nov 20"=>"bla", "Nov 24"=>"google.com" ', '("9174235567", "test@gmail.com", "louie@linkedin.com")', '{"Resume Review", "Mock Interview"}' , '{"Louisa", "Marcus"}', '{"Email Resume to Him", "Finish Zoo System Design Problem"}', 'Active');
+
+-- INSERT INTO connections (fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status) 
+-- VALUES ('Laura Glanz', 'Consultant', 'Accenture', 
+-- ' "Dec 9" => "google.com", "Dec 10" => "linkedincom", "Nov 20"=>"bla", "Nov 24"=>"google.com" ', '("9174235567", "test@gmail.com", "laura@linkedin.com")', '{"Resume Review", "Referral"}' , '{"Louisa", "Dave", "Chuck"}', '{"Email Resume to Her"}', 'Active');
+
+-- SELECT STATEMENTS FOR connections TABLE (working)
+-- SELECT contact_dates -> 'Dec 10' AS dec10 FROM connections WHERE fullname='Liam Jolley'; (without the WHERE it returns all contact_dates of Dec 10)
+-- For more info see : https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-hstore/
+
+
+-- Many to Many table of user accesses to other users dashboards
+CREATE TABLE granted_accesses (
+)
+
 
 
--- backend/server/server.js
+++ backend/server/server.js
@@ -5,24 +5,28 @@ const cors = require('cors');
 //can postgres queries with the pool
 const pool = require('./db');
 
-
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(5000, () => {
+    console.log(`The server is up and running on port 5000`);
+})
 
-// ALL ROUTING BELOW 
+
+// ALL ROUTING FOR JOBS BELOW 
 // Create a job
 app.post("/jobs", async (req, res) => {
     try {
-        const { jobTitle, jobLocation, companyName, companyUrl, companyDesc, appStatus, appDate } = req.body;
+        const { role, role_link, city, state, country, company, company_link, description, connections, status, applied_on } = req.body;
         const newJob = await pool.query(
-            "INSERT INTO jobs (jobTitle, jobLocation, companyName, companyUrl, companyDesc, appStatus, appDate) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *", [jobTitle, jobLocation, companyName, companyUrl, companyDesc, appStatus, appDate]
+            "INSERT INTO jobs (role, role_link, city, state, country, company, company_link, description, connections, status, applied_on) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *", [role, role_link, city, state, country, company, company_link, description, connections, status, applied_on]
         );
         res.json(newJob.rows[0]);
     } catch (error) {
         console.error(error.message);
+        res.send("Addition of job unsuccessful");
     }
 })
 
@@ -51,10 +55,10 @@ app.get("/jobs/:id", async (req, res) => {
 app.put("/jobs/:id", async (req, res) => {
     try {
         const { id } = req.params;
-        const { jobTitle, jobLocation, companyName, companyUrl, companyDesc, appStatus, appDate } = req.body;
+        const { role, role_link, city, state, country, company, company_link, description, connections, status, applied_on, created_at } = req.body;
         const updateJob = await pool.query(
-            "UPDATE jobs SET jobTitle=$1, jobLocation=$2, companyName=$3, companyUrl=$4, companyDesc=$5, appStatus=$6, appDate=$7 WHERE id = $8",
-            [jobTitle, jobLocation, companyName, companyUrl, companyDesc, appStatus, appDate, id]
+            "UPDATE jobs SET role=$1, role_link=$2, city=$3, state=$4, country=$5, company=$6, company_link=$7, description=$8, connections=$9, status=$10, applied_on=$11, created_at=$12 WHERE id = $13",
+            [role, role_link, city, state, country, company, company_link, description, connections, status, applied_on, created_at, id]
         );
         res.json(`Job with id ${id} was updated to ${updateJob.rows[0]}`);
     } catch (error) {
@@ -90,6 +94,66 @@ app.get("/jobs", async (req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log(`The server is up and running on port 5000`);
+
+// ALL ROUTING FOR CONNECTIONS BELOW
+// Create a connection
+app.post("/connections", async (req, res) => {
+    try {
+        const { fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status } = req.body;
+        const newJob = await pool.query(
+            "INSERT INTO connections (fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *", [fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status]
+        );
+        res.json(newJob.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+        res.send("Addition of connection unsuccessful");
+    }
 })
+
+// Get all connections
+app.get("/connections", async (req, res) => {
+    try {
+        const allJobs = await pool.query("SELECT * FROM connections");
+        res.json(allJobs.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+// Get a connection
+app.get("/connections/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const job = await pool.query("SELECT * FROM connections WHERE id = $1", [id]);
+        res.json(job.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+// Update a connection
+app.put("/connections/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status } = req.body;
+        const updateJob = await pool.query(
+            "UPDATE connections SET fullname=$1, role=$2, company=$3, contact_dates=$4, contact_information=$5, offers=$6, secondary_cons=$7, homework=$8, status=$9 WHERE id = $10",
+            [fullname, role, company, contact_dates, contact_information, offers, secondary_cons, homework, status, id]
+        );
+        res.json(`Job with id ${id} was updated to ${updateJob.rows[0]}`);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+// Delete a connection
+app.delete("/connections/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteConnection = await pool.query("DELETE FROM connections WHERE id = $1", [id]);
+        res.json(`Connection with id ${id} was deleted`);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
